13ed7d1f25082b1284903984e521778234f2361b
OH-JOO-YEONG <brb1111@naver.com>
2020-08-24 16:22:51 +0900

    Movie app basic done

---
+++ build/precache-manifest.910b0bfe1e864cfcee5d81c17232c13c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b55ba984c6ccbf16b63131477844ae99",
+    "url": "/movie_app_basic/index.html"
+  },
+  {
+    "revision": "1361817851223fd08b68",
+    "url": "/movie_app_basic/static/css/main.7a39a897.chunk.css"
+  },
+  {
+    "revision": "6217fbb8006c63353932",
+    "url": "/movie_app_basic/static/js/2.96609a9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_basic/static/js/2.96609a9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1361817851223fd08b68",
+    "url": "/movie_app_basic/static/js/main.6f870eb2.chunk.js"
+  },
+  {
+    "revision": "712db0f07c25ab175eea",
+    "url": "/movie_app_basic/static/js/runtime-main.82b73a97.js"
+  }
+]);
